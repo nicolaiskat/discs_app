3602347392be4391ec7594ba32f4c1b6e32c6a34
nicolaiskat <jegerrockstar@gmail.com>
2022-03-15 17:26:05 +0100

    Kan køre alt kode på nodemon server. ikke længere liveserver

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const fs = require('fs');
+const urlPath = fs.realpathSync('public');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    const indexPath = fs.realpathSync('index.html');
-    fs.readFile(indexPath, 'utf8', function(err, content){
+router.get(["/", "/index"], (req, res) => {
+    fs.readFile(urlPath + "/index.html", 'utf8', function(err, content){
         res.send(content);
     });
 });
+
+
+router.get("/list", (req, res) => {
+    fs.readFile(urlPath + "/list.html", 'utf8', function(err, content){
+        res.send(content);
+    });
+});
+
+
+router.get("/search", (req, res) => {
+    fs.readFile(urlPath + "/search.html", 'utf8', function(err, content){
+        res.send(content);
+    });
+});
+
+
+router.get("/taske", (req, res) => {
+    fs.readFile(urlPath + "/taske.html", 'utf8', function(err, content){
+        res.send(content);
+    });
+});
+
+
+router.get("/visu", (req, res) => {
+    fs.readFile(urlPath + "/visu.html", 'utf8', function(err, content){
+        res.send(content);
+    });
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 var cors = require('cors');
 const homeRoute = require('./routes/home');
 const discsRoute = require('./routes/discs');
-const mydiscsRoute = require('./routes/mydiscs')
+//const mydiscsRoute = require('./routes/mydiscs')
 
 const app = express();
 app.use(express.json());
 app.use(cors({origin: '*'}));
 app.use('/', homeRoute);
 app.use('/api/discs', discsRoute);
-app.use('/api/mydiscs', mydiscsRoute)
+//app.use('/api/mydiscs', mydiscsRoute)
 
 app.listen(3000, () => console.log("Server ready"));
