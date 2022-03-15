a21f4ee19558bbf7c5338ac3130b5105ab05b140
nicolaiskat <jegerrockstar@gmail.com>
2022-03-15 18:12:15 +0100

    tilføjet kommentarer på mit kode

--- routes/home.js
+++ routes/home.js
@@ -1,42 +1,36 @@
 const express = require("express");
-const fs = require('fs');
-const urlPath = fs.realpathSync('public');
+const fs = require("fs");
+const path = require("path");
+const urlPath = fs.realpathSync("public");
 
 const router = express.Router();
 
+//READING HTML PAGE FROM REQUESTED URL
 router.get(["/", "/index"], (req, res) => {
-    fs.readFile(urlPath + "/index.html", 'utf8', function(err, content){
-        res.send(content);
-    });
+  fs.readFile(urlPath + "/index.html", "utf8", function (err, content) {
+    res.send(content);
+  });
 });
-
-
 router.get("/list", (req, res) => {
-    fs.readFile(urlPath + "/list.html", 'utf8', function(err, content){
-        res.send(content);
-    });
+  fs.readFile(urlPath + "/list.html", "utf8", function (err, content) {
+    res.send(content);
+  });
 });
-
-
 router.get("/search", (req, res) => {
-    fs.readFile(urlPath + "/search.html", 'utf8', function(err, content){
-        res.send(content);
-    });
+  fs.readFile(urlPath + "/search.html", "utf8", function (err, content) {
+    res.send(content);
+  });
 });
-
-
 router.get("/taske", (req, res) => {
-    fs.readFile(urlPath + "/taske.html", 'utf8', function(err, content){
-        res.send(content);
-    });
+  fs.readFile(urlPath + "/taske.html", "utf8", function (err, content) {
+    res.send(content);
+  });
 });
-
-
 router.get("/visu", (req, res) => {
-    fs.readFile(urlPath + "/visu.html", 'utf8', function(err, content){
-        res.send(content);
-    });
+  fs.readFile(urlPath + "/visu.html", "utf8", function (err, content) {
+    res.send(content);
+  });
 });
 
-
+//EXPORTING ROUTER TO MODULE FOR USE IN SERVER.JS
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
-var cors = require('cors');
-const homeRoute = require('./routes/home');
-const discsRoute = require('./routes/discs');
-//const mydiscsRoute = require('./routes/mydiscs')
+var cors = require("cors");
+const homeRoute = require("./routes/home");
+const discsRoute = require("./routes/discs");
+//const mydiscsRoute = require('./routes/mydiscs');
 
 const app = express();
-app.use(express.json());
-app.use(cors({origin: '*'}));
-app.use('/', homeRoute);
-app.use('/api/discs', discsRoute);
-//app.use('/api/mydiscs', mydiscsRoute)
 
-app.listen(3000, () => console.log("Server ready"));
+//DEFINING DEFAULT CONTENT TYPE AS JSON FORMAT
+//ADDING CORS
+//USING PUBLIC FOLDER AS STATIC FOR HTML USE .CSS FILES
+//ADDING ROUTES TO WEPAPI
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(express.static("public"));
+app.use("/", homeRoute);
+app.use("/api/discs", discsRoute);
+//app.use('/api/mydiscs', mydiscsRoute);
+
+//STARTING APP ON SPECIFIED PORT
+const port = 3000;
+app.listen(port, () => console.log(`Server ready: http://localhost:${port}/`));
